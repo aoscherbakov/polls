5219f086a66edba7145446a35a05c935bf7a7597
Aleksey Shcherbakov <aoscherbakov@gmail.com>
2020-02-16 16:54:26 +0300

    add detail,results,vote views and urls

--- polls/views.py
+++ polls/views.py
@@ -7,4 +7,15 @@ def index(request):
     return HttpResponse("Hello World")
 
 
+def detail(request, question_id):
+    return HttpResponse("Your looking at question {}".format(question_id))
+
+def results(request, question_id):
+    return HttpResponse("Your looking at the results of question {}".format(question_id))
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question {}".format(question_id))
+
+
+
 
